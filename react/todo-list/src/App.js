e29190736cf2357c5bc57e8fbf5a638701ebf456
mk-tnd <mickey_saejia@hotmail.com>
2021-03-03 10:34:33 +0700

    update todo-list

--- react/todo-list/src/App.js
+++ react/todo-list/src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    list: ["To do 1", "To do 2", "To do 3"],
+  };
+
+  // constructor(props) {
+  //   super(props)
+  //   this.state = {
+  //     list:[],
+  //   }
+  // }
+
+  render() {
+    console.log(this.state);
+    return (
+      <div>
+        <input type="text" />
+        <button>ADD</button>
+        <ul>
+          {this.state.list.map((item) => (
+            <li>
+              {item}
+              <button>EDIT</button>
+              <button>DEL</button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
