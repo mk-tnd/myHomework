785e7c5e67e10735db817b9737feafce89919dc7
mk-tnd <mickey_saejia@hotmail.com>
2021-03-03 11:16:30 +0700

    commit

--- react/todo-list/src/App.js
+++ react/todo-list/src/App.js
@@ -1,9 +1,27 @@
+/** @jsxImportSource @emotion/react */
 import React from "react";
+import { css } from "@emotion/react";
 
 class App extends React.Component {
   state = {
-    list: ["To do 1", "To do 2", "To do 3"],
+    list: [
+      { task: "To do 1", status: true },
+      { task: "To do 2", status: false },
+      { task: "To do 3", status: true },
+    ],
+    text: "",
   };
+
+  handleValue = (e) => {
+    this.setState({ text: e.target.value });
+    console.log(e.target.value);
+  };
+
+  handleAdd = () => {};
+
+  handleEdit = () => {};
+
+  handleDelete = () => {};
 
   // constructor(props) {
   //   super(props)
@@ -16,12 +34,24 @@ class App extends React.Component {
     console.log(this.state);
     return (
       <div>
-        <input type="text" />
+        <input
+          type="text"
+          value={this.state.text}
+          onChange={this.handleValue}
+        />
         <button>ADD</button>
         <ul>
           {this.state.list.map((item) => (
             <li>
-              {item}
+              <span
+                style={{
+                  marginRight: "20px",
+                  textDecoration: item.status ? "line-through" : "none",
+                }}
+              >
+                {item.task}
+              </span>
+
               <button>EDIT</button>
               <button>DEL</button>
             </li>
